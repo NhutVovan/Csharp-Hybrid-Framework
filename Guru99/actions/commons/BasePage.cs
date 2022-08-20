cd6409363249e8310f83a03d3c49a515d9b59ca0
NhutVo <nhutvovan@gmail.com>
2022-08-20 20:19:35 +0700

    Build BasePage for Framework

--- Guru99/actions/commons/BasePage.cs
+++ Guru99/actions/commons/BasePage.cs
@@ -1,64 +1,427 @@
-﻿using NUnit.Framework;
+﻿using Hybrid_Framework.actions.commons;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
 namespace HybridFramework.actions.commons
 {
     internal class BasePage
     {
-        public static BasePage getBasePageObject()
+        public static BasePage GetBasePageObject()
         {
             return new BasePage();
         }
 
-        public void openPageUrl(WebDriver driver, String Url)
+        public void OpenPageUrl(IWebDriver driver, String Url)
         {
            driver.Url = Url;
         }
 
-        public String getPageTitle(WebDriver driver)
+        public String GetPageTitle(IWebDriver driver)
         {
             return driver.Title;
         }
 
-        public String getPageSource(WebDriver driver)
+        public String GetPageSource(IWebDriver driver)
         {
             return driver.PageSource;
         }
 
-        public void backToPage(WebDriver driver)
+        public void BackToPage(IWebDriver driver)
         {
             driver.Navigate().Back();
         }
 
-        public void forwardToPage(WebDriver driver)
+        public void ForwardToPage(IWebDriver driver)
         {
             driver.Navigate().Forward();
         }
 
-
-
-
-
-        private By getByXpath(String xpathLocator)
+        public void RefreshCurrentPage(IWebDriver driver)
         {
+            driver.Navigate().Refresh();
+        }
 
+        public IAlert WaitForAlertPresence(IWebDriver driver)
+        {
+            WebDriverWait explicitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(GlobalConstants.LONG_TIMEOUT));
+            return explicitWait.Until(ExpectedConditions.AlertIsPresent());
+        }
+
+        public void AcceptAlert(IWebDriver driver)
+        {
+            IAlert alert = WaitForAlertPresence(driver);
+            alert.Accept();
+        }
+
+        public void CancelAlert(IWebDriver driver)
+        {
+            IAlert alert = WaitForAlertPresence(driver);
+            alert.Dismiss();
+        }
+
+        public String GetAlertText(IWebDriver driver)
+        {
+            IAlert alert = WaitForAlertPresence(driver);
+            return alert.Text;
+        }
+
+        public void SendkeyToAlert(IWebDriver driver, String textVale)
+        {
+            IAlert alert = WaitForAlertPresence(driver);
+            alert.SendKeys(textVale);
+        }
+
+        public void SwichToWindowByID(IWebDriver driver, String windowID)
+        {
+            ReadOnlyCollection<String> allWindowIDs = driver.WindowHandles;
+
+            foreach (String id in allWindowIDs)
+            {
+                if (!id.Equals(windowID))
+                {
+                    driver.SwitchTo().Window(id);
+                    break;
+                }
+            }
+        }
+
+        public void SwichToWindowByTitle(IWebDriver driver, String tabTitle)
+        {
+            ReadOnlyCollection<String> allWindowIDs = driver.WindowHandles;
+
+            foreach (String id in allWindowIDs)
+            {
+                driver.SwitchTo().Window(id);
+                String actualTitle = driver. Title;
+                if (actualTitle.Equals(tabTitle))
+                {
+                    break;
+                }
+            }
+        }
+
+        public void CloseAllTabWithoutParent(IWebDriver driver, String parentID)
+        {
+            ReadOnlyCollection<String> allWindowIDs = driver.WindowHandles;
+
+            foreach (String id in allWindowIDs)
+            {
+                if (!id.Equals(parentID))
+                {
+                    driver.SwitchTo().Window(parentID);
+                    driver.Close();
+                }
+                driver.SwitchTo().Window(parentID);
+            }
+        }
+
+        private By GetByCssSelector(String cssLocator)
+        {
+            return By.CssSelector(cssLocator);
+        }
+
+        public void WaitForElementVisibleCssSelector(IWebDriver driver, String cssLocator)
+        {
+            WebDriverWait explicitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(GlobalConstants.LONG_TIMEOUT));
+            var element = explicitWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(cssLocator)));
+        }
+
+        private By GetByXpath(String xpathLocator)
+        {
             return By.XPath(xpathLocator);
         }
 
-        public void waitForElementVisible(WebDriver driver, String cssLocator)
+        private String GetDynamicXpath(String xpathLocator, String? dynamicValues)
         {
-            WebDriverWait explicitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-            var element = explicitWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(cssLocator)));
+            xpathLocator = String.Format(xpathLocator, dynamicValues);
+            return xpathLocator;
         }
+
+        protected IWebElement GetWebElement(IWebDriver driver, String xpathLocator)
+        {
+            return driver.FindElement(GetByXpath(xpathLocator));
+        }
+
+        public ReadOnlyCollection<IWebElement> GetListWebElement(IWebDriver driver, String xpathLocator)
+        {
+            return driver.FindElements(GetByXpath(xpathLocator));
+        }
+
+        public void ClickToElement(IWebDriver driver, String xpathLocator)
+        {
+            GetWebElement(driver, xpathLocator).Click();
+        }
+
+        public void ClickToElement(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)).Click();
+        }
+
+        public void SendkeyToElement(IWebDriver driver, String xpathLocator, String textValue)
+        {
+            IWebElement element = GetWebElement(driver, xpathLocator);
+            element.Clear();
+            element.SendKeys(textValue);
+        }
+
+        public void SendkeyToElement(IWebDriver driver, String xpathLocator, String textValue, String? dynamicValues)
+        {
+            IWebElement element = GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues));
+            element.Clear();
+            element.SendKeys(textValue);
+        }
+
+        public void SelectItemInDefaultDropdown(IWebDriver driver, String xpathLocator, String textItem)
+        {
+            SelectElement select = new SelectElement(GetWebElement(driver, xpathLocator));
+            select.SelectByText(textItem);
+        }
+
+        public void SelectItemInDefaultDropdown(IWebDriver driver, String xpathLocator, String textItem, String? dynamicValues)
+        {
+            SelectElement select = new SelectElement(GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)));
+            select.SelectByText(textItem);
+        }
+
+        public Boolean IsDropdownMultiple(IWebDriver driver, String xpathLocator)
+        {
+            SelectElement select = new SelectElement(GetWebElement(driver, xpathLocator));
+            return select.IsMultiple;
+        }
+
+        public void SelectItemInCustomDropdown(IWebDriver driver, String parentXpath, String childXpath, String expectedTextItem)
+        {
+            GetWebElement(driver, parentXpath).Click();
+            SleepInSecond(1);
+
+            WebDriverWait explicitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(GlobalConstants.LONG_TIMEOUT));
+
+            ReadOnlyCollection<IWebElement> allItems = explicitWait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(GetByXpath(childXpath)));
+            foreach (IWebElement item in allItems)
+            {
+                if (item.Text.Trim().Equals(expectedTextItem))
+                {
+                    IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+                    jsExecutor.ExecuteScript("arguments[0].scrollIntoView(true);", item);
+                    SleepInSecond(1);
+                    item.Click();
+                    break;
+                }
+            }
+        }
+
+        public void SleepInSecond(long second)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(second));
+        }
+
+        public String GetElementAttribute(IWebDriver driver, String xpathLocator, String AttributeName)
+        {
+            return GetWebElement(driver, xpathLocator).GetAttribute(AttributeName);
+        }
+
+        public String GetElementAttribute(IWebDriver driver, String xpathLocator, String AttributeName, String? dynamicValues)
+        {
+            return GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)).GetAttribute(AttributeName);
+        }
+
+        public String GetElementText(IWebDriver driver, String xpathLocator)
+        {
+            return GetWebElement(driver, xpathLocator).Text;
+        }
+
+        public String GetElementText(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            return GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)).Text;
+        }
+
+        public String GetCssValue(IWebDriver driver, String xpathLocator, String PropertyName)
+        {
+            return GetWebElement(driver, xpathLocator).GetCssValue(PropertyName);
+        }
+
+        public int GetElementSize(IWebDriver driver, String xpathLocator)
+        {
+            return GetListWebElement(driver, xpathLocator).Count;
+        }
+
+        public int GetElementSize(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            return GetListWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)).Count;
+        }
+
+        public void CheckToDefaultCheckboxOrRadio(IWebDriver driver, String xpathLocator)
+        {
+            IWebElement element = GetWebElement(driver, xpathLocator);
+            if (!element.Selected)
+            {
+                element.Click();
+            }
+        }
+
+        public void CheckToDefaultCheckboxOrRadio(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            IWebElement element = GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues));
+            if (!element. Selected)
+            {
+                element.Click();
+            }
+        }
+
+        public void UncheckToDefaultCheckbox(IWebDriver driver, String xpathLocator)
+        {
+            IWebElement element = GetWebElement(driver, xpathLocator);
+            if (element.Selected)
+            {
+                element.Click();
+            }
+        }
+
+        public void UncheckToDefaultCheckbox(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            IWebElement element = GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues));
+            if (element.Selected)
+            {
+                element.Click();
+            }
+        }
+
+        public Boolean IsElementDisplayed(IWebDriver driver, String xpathLocator)
+        {
+            return GetWebElement(driver, xpathLocator).Displayed;
+        }
+
+        public Boolean IsElementDisplayed(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            return GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)).Displayed;
+        }
+
+        public void OverrideImplicitTimeout(IWebDriver driver, long timeout)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeout);
+        }
+
+        public Boolean IsElementUndisplayed(IWebDriver driver, String xpathLocator)
+        {
+            OverrideImplicitTimeout(driver, GlobalConstants.SHORT_TIMEOUT);
+            ReadOnlyCollection<IWebElement> elements = GetListWebElement(driver, xpathLocator);
+            OverrideImplicitTimeout(driver, GlobalConstants.LONG_TIMEOUT);
+            if (elements.Count == 0)
+            {
+                return true;
+            }
+            else if (elements.Count > 0 && !elements.First().Displayed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean IsElementUndisplayed(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            OverrideImplicitTimeout(driver, GlobalConstants.SHORT_TIMEOUT);
+            ReadOnlyCollection<IWebElement> elements = GetListWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues));
+            OverrideImplicitTimeout(driver, GlobalConstants.LONG_TIMEOUT);
+            if (elements.Count == 0)
+            {
+                return true;
+            }
+            else if (elements.Count > 0 && !elements.First().Displayed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean IsElementEnabled(IWebDriver driver, String xpathLocator)
+        {
+            return GetWebElement(driver, xpathLocator).Enabled;
+        }
+
+        public Boolean IsElementSelected(IWebDriver driver, String xpathLocator)
+        {
+            return GetWebElement(driver, xpathLocator).Selected;
+        }
+
+        public void SwitchToFrameIframe(IWebDriver driver, String xpathLocator)
+        {
+            driver.SwitchTo().Frame(GetWebElement(driver, xpathLocator));
+        }
+
+        public void SwitchToDefaultContent(IWebDriver driver)
+        {
+            driver.SwitchTo().DefaultContent();
+        }
+
+        public void HoverMouseToElement(IWebDriver driver, String xpathLocator)
+        {
+            Actions action = new Actions(driver);
+            action.MoveToElement(GetWebElement(driver, xpathLocator)).Perform();
+        }
+
+        public void PressKeyToElement(IWebDriver driver, String xpathLocator, String key)
+        {
+            Actions action = new Actions(driver);
+            action.SendKeys(GetWebElement(driver, xpathLocator), key).Perform();
+        }
+
+        public void PressKeyToElement(IWebDriver driver, String xpathLocator, String key, String? dynamicValues)
+        {
+            Actions action = new Actions(driver);
+            action.SendKeys(GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues)), key).Perform();
+        }
+
+        public void ScrollToBottomPage(IWebDriver driver)
+        {
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("window.scrollBy(0,document.body.scrollHeight)");
+        }
+
+        public void HightlightElement(IWebDriver driver, String xpathLocator)
+        {
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            IWebElement element = GetWebElement(driver, xpathLocator);
+            String hightlightStyle = "border: 2px solid red; border-style:dashed";
+            String originalStyle = element.GetAttribute("style");
+            jsExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1],arguments[2])", element, hightlightStyle);
+            SleepInSecond(1);
+            jsExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1],arguments[2])", element, originalStyle);
+        }
+
+        public void HightlightElement(IWebDriver driver, String xpathLocator, String? dynamicValues)
+        {
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            IWebElement element = GetWebElement(driver, GetDynamicXpath(xpathLocator, dynamicValues));
+            String hightlightStyle = "border: 2px solid red; border-style:dashed";
+            String originalStyle = element.GetAttribute("style");
+            jsExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1],arguments[2])", element, "style", hightlightStyle);
+            SleepInSecond(1);
+            jsExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1],arguments[2])", element, "style", originalStyle);
+        }
+
+
 
     }
 }
---
+++ Guru99/actions/commons/GlobalConstants.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CoreUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid_Framework.actions.commons
+{
+    public static class GlobalConstants
+    {
+        public const int LONG_TIMEOUT = 30;
+        public const int SHORT_TIMEOUT = 5;
+
+    }
+}
