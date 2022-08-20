cca5c39e64320bda2ad773cfb96698cf4b95e400
NhutVo <nhutvovan@gmail.com>
2022-08-20 14:41:43 +0700

    Add project files.

---
+++ Guru99/actions/commons/BasePage.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HybridFramework.actions.commons
+{
+    internal class BasePage
+    {
+        public static BasePage getBasePageObject()
+        {
+            return new BasePage();
+        }
+
+        public void openPageUrl(WebDriver driver, String Url)
+        {
+           driver.Url = Url;
+        }
+
+        public String getPageTitle(WebDriver driver)
+        {
+            return driver.Title;
+        }
+
+        public String getPageSource(WebDriver driver)
+        {
+            return driver.PageSource;
+        }
+
+        public void backToPage(WebDriver driver)
+        {
+            driver.Navigate().Back();
+        }
+
+        public void forwardToPage(WebDriver driver)
+        {
+            driver.Navigate().Forward();
+        }
+
+        public void refreshCurrentPage(WebDriver driver)
+        {
+            driver.Navigate().Refresh();
+        }
+
+        private By getByXpath(String xpathLocator)
+        {
+
+            return By.XPath(xpathLocator);
+        }
+
+        public void waitForElementVisible(WebDriver driver, String cssLocator)
+        {
+            WebDriverWait explicitWait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            var element = explicitWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(cssLocator)));
+        }
+    }
+}
